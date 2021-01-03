ad5ecd5e97547199b95032bbef9fc3ac6bc5ccad
김성민 <seongmin.kim@thingsflow.kr>
2021-01-03 17:46:37 +0900

    첫번째 예시 이모지 위젯 추가.

--- Game Status Part 1/EmojiRangersWidget/EmojiRangersWidget.swift
+++ Game Status Part 1/EmojiRangersWidget/EmojiRangersWidget.swift
@@ -1,6 +1,6 @@
 //
-//  EmojiRangersWidget.swift
-//  EmojiRangersWidget
+//  EmojiRangerWidget.swift
+//  EmojiRangerWidget
 //
 //  Created by Min on 2021/01/03.
 //  Copyright © 2021 Apple. All rights reserved.
@@ -11,25 +11,18 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), character: .panda)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+        let entry = SimpleEntry(date: Date(), character: .panda)
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        let entries: [SimpleEntry] = [SimpleEntry(date: Date(), character: .panda)]
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -37,32 +30,48 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let character: CharacterDetail
 }
 
-struct EmojiRangersWidgetEntryView : View {
+// 홈 화면에서 보여지는 화면
+struct EmojiRangerWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        AvatarView(entry.character)
+    }
+}
+
+// 플레이스 홀더 화면(추가해줌)
+struct PlaceholderView: View {
+    var body: some View {
+        AvatarView(.panda)
+            .redacted(reason: .placeholder)
+//            .isPlaceholder(true) // < 예시에서는 이렇게 나오는데 실제 존재하지않음..
     }
 }
 
 @main
-struct EmojiRangersWidget: Widget {
-    let kind: String = "EmojiRangersWidget"
+struct EmojiRangerWidget: Widget {
+    let kind: String = "EmojiRangerWidget"
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            EmojiRangersWidgetEntryView(entry: entry)
+            EmojiRangerWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Emoji Ranger Detail")
+        .description("Keep track of your favorite emoji ranger.")
+        .supportedFamilies([.systemSmall])
     }
 }
 
-struct EmojiRangersWidget_Previews: PreviewProvider {
+struct EmojiRangerWidget_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiRangersWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        Group {
+            AvatarView(.panda)
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+            PlaceholderView()
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+        }
     }
 }
